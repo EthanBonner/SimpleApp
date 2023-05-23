8c4468d2d4d6d2bc36bd89b35600c3465ea4899e
EthanBonner <ethanbonner85@gmail.com>
2023-05-23 15:33:36 -0700

    Initial Commit

---
+++ SimpleApp/SimpleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleAppApp.swift
+//  SimpleApp
+//
+//  Created by Ethan Bonner on 5/23/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
